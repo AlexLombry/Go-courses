c85157b542f8727e8e2e51e121f4ceb738448114
Alex Lombry <alex.lombry@gmail.com>
2018-12-08 14:23:06 +0100

    Comment for handling value vs references type for pointer

--- struct/main.go
+++ struct/main.go
@@ -23,13 +23,24 @@ func main() {
 		},
 	}
 
-	p := &joe
-	p.updateFirstName("Mike")
+	// &variable give the memory address the variable is pointing at
+	// p := &joe
+	//p.updateFirstName("Mike")
+	joe.updateFirstName("Mike")
 	joe.print()
 }
 
+// No pointer's needed for a slice, differ for a struct
+// There's two kind of data in go, value types and reference types.
+// References type is already a pointer by itself, so you don't need to use pointer for :
+// slices, maps, channels, pointers, functions.
+// But for all values types, you have to, eg :
+// int, float, string, bool, structs ...
+
 func (p *person) updateFirstName(firstname string) {
-	(*p).firstName = firstname
+	// *p give the value of the memory address it points
+	// (*p).firstName = firstname
+	p.firstName = firstname
 }
 
 func (p person) print() {
